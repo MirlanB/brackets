05d4dd361554d92e1a5bb05dd173ec2c8ccd2bdf
MirlanB <78996635+MirlanB@users.noreply.github.com>
2021-03-15 19:44:55 +0300

    fix:add func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+  
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const newStr = str.split('');
+  const storage = [];
+  const openBrackets = [];
+  const closeBrackets = [];
+  brackets = {};
+  if (Array.isArray(bracketsConfig[0])) {
+    bracketsConfig.map((item) => {
+      openBrackets.push(item[0]);
+      closeBrackets.push(item[1]);
+      brackets[item[1]] = item[0];
+    });
+  } else {
+    openBrackets.push(bracketsConfig[0]);
+    closeBrackets.push(bracketsConfig[1]);
+  }
+
+  for (let i = 0; i < newStr.length; i++) {
+    if (openBrackets.includes(newStr[i])) {
+      if (closeBrackets.includes(newStr[i])) {
+        if (newStr[i] === storage[storage.length - 1]) {
+          storage.pop();
+        } else storage.push(newStr[i]);
+      } else storage.push(newStr[i]);
+    } else if (brackets[newStr[i]] === storage[storage.length - 1]) {
+      storage.pop();
+    } else return false;
+  }
+  return storage.length === 0;
+};
